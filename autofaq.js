a69dc06f225bf15cf5c88f5bcbdbc042b592f01a
aishutin <hazmozavr@gmail.com>
2020-07-23 13:51:58 +0300

    v1.0
    Working frontend

--- autofaq.js
+++ autofaq.js
@@ -1,11 +1,175 @@
+class BertTokenizer {
+  constructor(vocab, special_tokens_map, tokenizer_config) {
+      this.tokenizer_config = tokenizer_config;
+      this.special_tokens_map = special_tokens_map;
+      this.vocab = vocab;
+      this.max_len = 0;
+      for (var key in vocab) {
+        if (this.max_len < key.length) this.max_len = key.length;
+      }
+      this.whitespaces = new Set([" ", "\n", "\t"]);
+  }
+
+  isLemma(token) {
+    if (token.length < 3) {
+      return false;
+    }
+    return token[0] === '#' && token[1] === '#';
+  }
+
+  formatToken(token) {
+    if (this.isLemma(token)) {
+      return token.slice(2);
+    }
+    return token;
+  }
+
+  encode(text, add_special_tokens=true) {
+    if (this.tokenizer_config['do_lower_case']) {
+      text = text.toLowerCase();
+    }
+
+    var tokens_ids = [];
+    if (add_special_tokens) {
+      /*console.log(this.special_tokens_map)
+      console.log(this.special_tokens_map.cls_token in this.vocab);*/
+      tokens_ids = [this.vocab[this.special_tokens_map.cls_token]]
+    }
+    var start = 0;
+    var is_new_word = true;
+
+    while (start < text.length) {
+      var longest_key = "";
+      // check if it's whitespace;
+      if (this.whitespaces.has(text[start])) {
+        is_new_word = true;
+        start += 1;
+        continue;
+      }
+      // search longest applicable token
+      //console.log('start: '+ start + " tokens_ids: " + tokens_ids)
+
+      var L = 0;
+      var R = this.max_len + 1;
+      //console.log('this.max_len ' + this.max_len)
+      while (R - L > 1) {
+        var med = (L + R) / 2;
+        var str = text.slice(start, start + med + 1);
+        // console.log(med + ' ' + str);
+
+        var flag = is_new_word && this.vocab.hasOwnProperty(str) ||
+                  !is_new_word && this.vocab.hasOwnProperty('##' + str);
+        if (flag) {
+          L = med;
+          longest_key = str;
+        } else {
+          R = med;
+        }
+      }
+
+      start += this.formatToken(longest_key).length;
+
+      if (longest_key === "") {
+        start += 1;
+        longest_key = this.special_tokens_map.unk_token;
+      }
+      //console.log(longest_key);
+      tokens_ids.push(this.vocab[longest_key]);
+
+    //  console.log('START ' + start);
+    }
+    tokens_ids.push(this.vocab[this.special_tokens_map.sep_token]);
+    //console.log(tokens_ids);
+    return tokens_ids;
+  }
+}
+
+function get_vocab(url) {
+  var text = "";
+  $.ajax({
+    'url': url,
+    'async': false,
+    'success': function (text1) {
+      text = text1;
+      return text1;
+      },
+  })
+  let arr = text.split('\n');
+  console.log(arr);
+  let vocab = {};
+  let ind = 0;
+  for (var i = 0; i < arr.length; i++) { // arr.length;
+    vocab[arr[i]] = i;
+  }
+  return vocab;
+}
+
+var defaultSpecialTokensMap = {"unk_token": "[UNK]", "sep_token": "[SEP]",
+                              "pad_token": "[PAD]", "cls_token": "[CLS]",
+                              "mask_token": "[MASK]"};
+var defaultTokenizerConfig = {"do_lower_case": true, "model_max_length": 512};
+
+function find_vocab() {
+  return get_vocab("./distilbert/vocab.txt");
+}
+let vocab = find_vocab();
+var tokenizer = new BertTokenizer(vocab, defaultSpecialTokensMap, defaultTokenizerConfig);
+
+async function createModel() {
+  const model = await tf.loadGraphModel('./mobilebert/web_model/model.json');
+  return model;
+}
+
+function prepare_input(text) {
+  // {input_ids=input_ids, input_mask=input_mask, segment_ids=segment_ids}
+  let input_ids = tokenizer.encode(text)
+  let input_mask = [];
+  let segment_ids = []
+  while (input_mask.length < input_ids.length) {
+    input_mask.push(1);
+    segment_ids.push(0);
+  }
+  tf.tensor(input_mask)
+  const len = input_ids.length
+  input_ids = tf.tensor(input_ids, shape=[1, len], dtype="int32"); // , dtype="int32"
+  input_mask = tf.tensor(input_mask, shape=[1, len], dtype="int32");
+  segment_ids = tf.tensor(segment_ids, shape=[1, len], dtype="int32");
+  //console.log(input_ids)
+  return {
+    "input_ids": input_ids,
+    "input_mask": input_mask,
+    "segment_ids": segment_ids
+  }
+}
+
+function get_embeddings(outputs) {
+  // console.log('embedding')
+  // console.log(outputs)
+  var norm = tf.norm(outputs, ord='euclidean', axis=1, keepdims=true);
+  // console.log(norm)
+  var result = tf.div(outputs, norm);
+  // console.log(result);
+  norm.dispose()
+  return result;
+}
+
+var MODEL = undefined;
+
+function run_model(text) {
+  // console.log('modeling ' + text);
+  const inputs = prepare_input(text)
+  const results = MODEL.execute(inputs)[0];
+  const embeddings = get_embeddings(results);
+  // console.log(embeddings);
+  results.dispose();
+  return embeddings;
+}
+
 document.TEXT_CACHE = {}
 
 function addAllTextToCache() {
-  console.log('real scanning')
   $("p").each(function() {
     const text = $(this).text();
-    console.log("next <p> element with text: " + text);
-    console.log(document.TEXT_CACHE.hasOwnProperty(text));
     if (document.TEXT_CACHE.hasOwnProperty(text)) {
       return;
     }
@@ -20,17 +184,16 @@ function findRelevant(text, n=1) {
     if (document.TEXT_CACHE.hasOwnProperty(key)) {
       var product_tensor = tf.mul(vector, document.TEXT_CACHE[key]);
       var score_tensor = product_tensor.sum();
-      results.push([-score_tensor.dataSync()[0], key]);
+      results.push([score_tensor.dataSync()[0], key]);
       product_tensor.dispose();
       score_tensor.dispose();
     }
   }
+  console.log('results');
+  console.log(results);
   console.log(vector);
   vector.dispose();
-  results.sort();
-  for (var i = 0; i < n && i < results.length; i++) {
-    results[i][0] *= -1;
-  }
+  results.sort(function(a, b){return b[0] - a[0]});
   return results.slice(0, n);
 }
 
@@ -102,9 +265,12 @@ function addAutoFAQChat() {
 }
 
 function setupAutoFAQ() {
-  addAllTextToCache();
-  addAutoFAQLogo();
-  addAutoFAQChat();
+  createModel().then(model => {
+    MODEL = model;
+    addAllTextToCache();
+    addAutoFAQLogo();
+    addAutoFAQChat();
+  });
 }
 
 setupAutoFAQ();
